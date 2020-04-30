ed9c662ae1147ee008ba328b92aaea7794299c7d
mikeonator <maudinyc@gmail.com>
2020-04-29 21:26:42 -0400

    Finalized input recording in swagbot

--- GoslingUtils-master/SwagBot.py
+++ GoslingUtils-master/SwagBot.py
@@ -18,28 +18,41 @@ class SwagBot(GoslingAgent):
             agent.inputsave = {"AButton":[], "BButton":[], "RightBump":[], "RightTrig":[], "LeftTrig":[], "Yaw":[], "Pitch":[], "Time":[]}
             indict = open(os.getcwd() + "/SavedOutput.txt","r")
             agent.inputsave = ast.literal_eval(indict.read())
-            count = 0
+            BButton = False
+            AButton = False
+            RightBump = False
+            LeftTrig = False
+            RightTrig = False
+            Yaw = False
+            Pitch = False
+
             for event in events:
                 if event.code == "BTN_EAST":
                     agent.inputsave["BButton"].append(event.state)
-                    count += 1
+                    BButton = True
+
                 if event.code == "BTN_SOUTH":
+                    AButton = True
                     agent.inputsave["AButton"].append(event.state)
-                    count += 1
-                if event.code == "ButtonBTN_TR":
+
+                if event.code == "BTN_TR":
+                    RightBump = True
                     agent.inputsave["RightBump"].append(event.state)
-                    count += 1
-                if event.code == "ButtonABS_Z":
+
+                if event.code == "ABS_Z":
+                    LeftTrig = True
                     savestate = int(event.state)
                     outstate = (savestate)/255
                     agent.inputsave["LeftTrig"].append(outstate)
-                    count += 1
-                if event.code == "ButtonABS_RZ":
+
+                if event.code == "ABS_RZ":
+                    RightTrig = True
                     savestate = int(event.state)
                     outstate = (savestate)/255
                     agent.inputsave["RightTrig"].append(outstate)
-                    count += 1
-                if event.code == "ButtonABS_X":
+
+                if event.code == "ABS_X":
+                    Yaw = True
                     savestate = int(event.state)
                     outstate = float()
                     if savestate > 0:
@@ -47,8 +60,9 @@ class SwagBot(GoslingAgent):
                     else:
                         outstate = (savestate)/32768
                     agent.inputsave["Yaw"].append(outstate)
-                    count += 1
-                if event.code == "ButtonABS_Y":
+
+                if event.code == "ABS_Y":
+                    Pitch = True
                     savestate = int(event.state)
                     outstate = float()
                     if savestate > 0:
@@ -56,9 +70,32 @@ class SwagBot(GoslingAgent):
                     else:
                         outstate = (savestate)/32768
                     agent.inputsave["Pitch"].append(outstate)
-                    count += 1
-            if count != 0:
+            
+            if (AButton or BButton or RightBump or LeftTrig or RightTrig or Pitch or Yaw):
+                print('brih')
                 agent.inputsave["Time"].append((agent.time - agent.inputsave['Time'][0]))
+
+                if (not (AButton)):
+                    agent.inputsave["AButton"].append(0)
+                
+                if (not (BButton)):
+                    agent.inputsave["BButton"].append(0)
+                
+                if (not (RightBump)):
+                    agent.inputsave["RightBump"].append(0)
+                
+                if (not (LeftTrig)):
+                    agent.inputsave["LeftTrig"].append(0)
+                
+                if (not (RightTrig)):
+                    agent.inputsave["RightTrig"].append(0)
+                
+                if (not (Pitch)):
+                    agent.inputsave["Pitch"].append(0)
+                
+                if (not (Yaw)):
+                    agent.inputsave["Yaw"].append(0)
+
             outF = open(os.getcwd() + "/SavedOutput.txt", "w")
             outF.write(str(agent.inputsave))
             outF.close()
