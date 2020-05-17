7b0e982f1b0f527a7827dc4df1eef7e6351fe22d
mikeonator <maudinyc@gmail.com>
2020-05-17 15:53:18 -0400

    updated swagbot

--- GoslingUtils-master/SwagBot.py
+++ GoslingUtils-master/SwagBot.py
@@ -12,9 +12,18 @@ class SwagBot(GoslingAgent):
             if agent.kickoff_flag:
                 agent.push(kickoff())
             else:
-                close = ((agent.me.location - agent.ball.location).magnitude() > 2000)
+
+                close = ((agent.me.location - agent.ball.location).magnitude() < 2000)
                 need_boost = (agent.me.boost < 20)
-                if need_boost:
+                my_goal_to_ball,ball_distance = (agent.ball.location - agent.friend_goal.location).normalize(True)
+                goal_to_me = agent.me.location - agent.friend_goal.location
+                my_distance = my_goal_to_ball.dot(goal_to_me)
+
+
+                me_offside = my_distance - 200 > ball_distance
+                foe_offside = abs(agent.foe_goal.location.y - agent.foes[0].location.y) - 200 > abs(agent.foe_goal.location.y - agent.ball.location.y)
+
+                if need_boost and foe_offside:
                     large_boosts = [boost for boost in agent.boosts if boost.large and boost.active]
                     if (len(large_boosts) > 0):
                         closest_fatboost = large_boosts[0]
@@ -22,7 +31,10 @@ class SwagBot(GoslingAgent):
                                 if (boost.location - agent.me.location).magnitude() < (closest_fatboost.location - agent.me.location).magnitude():
                                     closest_fatboost = boost
                         agent.push(goto_boost(closest_fatboost))   
-                else:
+                elif(not(agent.kickoff_flag)):
+                    targets = {"goal":(opponent_left_post,opponent_right_post), "anywhere_but_my_net":(my_right_post,my_left_post)}
+                    hits = find_hits(agent,targets)
+                    print(hits)
                     relative_target = agent.ball.location - agent.me.location
                     local_target = agent.me.local(relative_target)
                     defaultPD(agent, local_target)
